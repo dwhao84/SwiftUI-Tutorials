a7fb8edea4eb72fd9fd6e52bb2ff85941c0846df
Dawei Hao <dwsamurai84@gmail.com>
2024-06-09 23:46:52 +0800

    將現有的兩個VStack 用Computed property 的方式，使用function帶入畫面中

--- SwiftUI Tutorials/WeatherForecast.swift
+++ SwiftUI Tutorials/WeatherForecast.swift
@@ -7,12 +7,58 @@
 
 import SwiftUI
 
+/*
+ HStack means horiztal stack, it could contains two different view.
+ And this time we use the computed Property to build a view.
+ */
+
 struct WeatherForecast: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+                DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+                DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        }
     }
 }
 
 #Preview {
     WeatherForecast()
+}
+
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconStyle: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(day)")
+                .font(.title2)
+            Image(systemName: iconName)
+                .foregroundStyle(iconStyle)
+                .imageScale(.large)
+            Text("High: \(high)")
+                .font(.title2)
+            Text("Low: \(low)")
+                .font(.title2)
+        }
+    }
 }
